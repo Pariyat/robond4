2591bed0b84738d24a404631399083ee431acf70
Pariyat <macchiato.me@gmail.com>
2019-08-03 18:58:35 -0700

    2nd submit

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -11,14 +11,16 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    	// TODO: Request a service and pass the velocities to it to drive the robot
 	ROS_INFO_STREAM("Driving the robot to the target.");
+	
 	ball_chaser::DriveToTarget srv;
     	srv.request.linear_x = lin_x;
     	srv.request.angular_z = ang_z;
-    if (!client.call(srv)) {
-	    ROS_ERROR("Failed to call service DriveToTarget.");
-}
+    	
+	if (!client.call(srv)) {
+	    	ROS_ERROR("Failed to call service DriveToTarget.");
+	}
 }
 
 
@@ -27,40 +29,67 @@ void drive_robot(float lin_x, float ang_z)
 void process_image_callback(const sensor_msgs::Image img)
 {
 
-    int white_pixel = 255;
+    	int white_pixel = 255;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-	int height = img.height;
-    	int step = img.step;
+	
+	bool found_ball = false;
+	
+	int row = 0;
+	int step = 0;
+	int i = 0;
 
-    float x = 0.0;
-    float z = 0.0;
-  
-    float offset_accumulated = 0;
-    int count_total = 0;
+	//int height = img.height;
+    	//int step = img.step;
 
-    for (int i = 0; i < height ; i++) {
-        for (int j = 0; j < step; j++) {
-            if (img.data[i * step + j] == white_pixel) {
-                offset_accumulated += j - step / 2.0;
-                count_total++;
-            }
-        }
-    }
+    	float x = 0.0;
+    	float z = 0.0;
+  	
+	for (row = 0; row < img.height && found_ball == false; row++)
+    	{
+        	for (step = 0; step < img.step && found_ball == false; ++step)
+        	{   
+            		i = (row*img.step)+step;
+            		//ROS_INFO("row: %d, step: %d, i: %d", row, step, i);
+            		if (img.data[i] == white_pixel)
+			{   
+				found_ball = true;
+				//ROS_INFO("row: %d, step: %d, i: %d", row, step, i);
+				
+			 }
+		}
+    	}	
 
- if (count_total == 0) {
-        x = 0.0;
-        z = 0.0;
-    }
-    else {
-        x = 0.1;
-        z = -4.0 * offset_accumulated / count_total / (step /2.0);
-    }
-
-drive_robot(x, z);
+    	 if (found_ball)
+    	{
+        // Then, identify if this pixel falls in the left, mid, or right side of the image
+        	int imgThird = img.width/3;
+        	int col = step/3;
+        //ROS_INFO("col: %d", col);
+        	if (col < imgThird) 
+        	{
+        	    drive_robot(0.1, 0.1);
+            //ROS_INFO("LEFT");
+        	} 
+        	else if (col >= imgThird && col < 2*imgThird)
+        	{
+        	    drive_robot(0.5, 0.0);
+            //ROS_INFO("MID");
+        	}
+        	else if (col >= 2*imgThird)
+        	{
+            	drive_robot(0.1, -0.1);
+            //ROS_INFO("RIGHT");
+        	}
+        // Depending on the white ball position, call the drive_bot function and pass velocities to it
+    	}
+	else
+	{
+		drive_robot(x, z);
+	}
 }
 
 int main(int argc, char** argv)

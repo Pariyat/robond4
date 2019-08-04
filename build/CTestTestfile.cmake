16ffef3c0eec93df1343859bc0b5442a2d26e0f3
Pariyat <macchiato.me@gmail.com>
2019-08-04 05:25:20 -0700

    submit 4th

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/robo-nd-ws/Project2/src
-# Build directory: /home/robond/robo-nd-ws/Project2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(my_robot)
-subdirs(ball_chaser)
--- build/ball_chaser/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/robond/robo-nd-ws/Project2/install/include".split(';') if "/home/robond/robo-nd-ws/Project2/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "ball_chaser"
-PROJECT_SPACE_DIR = "/home/robond/robo-nd-ws/Project2/install"
-PROJECT_VERSION = "0.0.0"
